0bec9a0502c9545522df8989dd064b7045a5d69d
lotar37 <lot-work@yandex.ru>
2020-01-27 23:25:14 +0300

    мышь плывет над холстом

--- catch_the_ball/game_canvas.py
+++ catch_the_ball/game_canvas.py
@@ -1,1 +1,13 @@
 import tkinter
+def paint(event):
+    print(event.x, event.y)
+
+root = tkinter.Tk()
+
+canvas = tkinter.Canvas(root)
+canvas.pack()
+canvas.bind("<Motion>",paint)
+
+
+root.mainloop()
+print("Тут и сказочке конец")
