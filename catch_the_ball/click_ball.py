560587a67aea82bd8ad4fa0af7518e57d0b30468
lothomecomp <lotar73@gmail.com>
2020-01-29 23:40:48 +0300

    Игра "Лови и дави"!!

--- catch_the_ball/click_ball.py
+++ catch_the_ball/click_ball.py
@@ -35,6 +35,9 @@ class MainWindow:
             canvas.itemconfig(self.scores_text, text=scores_format%self.scores)
             self.scores_text
             self.ball.destroy()
+
+
+
             self.ball = Ball.generate_random_ball()
 
     def game_cycle(self, *ignore):
