c24596818a97db624234a83aadffd635b1b5ff25
lothomecomp <lotar73@gmail.com>
2020-01-29 15:19:16 +0300

    Подготовка описаний процедур, необходимых для игры

--- catch_the_ball/game_canvas.py
+++ catch_the_ball/game_canvas.py
@@ -3,13 +3,38 @@ def paint(event):
     print(event.x, event.y)
     canvas.coords(line, 0,0,event.x, event.y)
 
-root = tkinter.Tk()
+def create_random_ball():
+    canvas.create_oval(x, y, x+2*R, y+2*R, fill=random_color, outline='white')
 
-canvas = tkinter.Canvas(root)
-canvas.pack()
-canvas.bind("<Motion>",paint)
-canvas.create_oval(150,10,190,50, fill='orange', outline='white')
-line = canvas.create_line(100, 175, 100, 50, fill='lightblue',width=1)
+def init_ball_catch_game():
+    """
+    создает необходимоео для игры колличество шариков
 
+    :return:
+    """
+
+def click_ball():
+    """
+    по клику удаляет шарик над которым мышь
+    :return:
+    """
+
+def create_random_ball()
+    """
+    создает шарик в случайном месте холста
+    при этом шарик не выходит за границы canvas
+    :return: 
+    """
+
+def init_main_window():
+    global root, canvas
+    root = tkinter.Tk()
+
+    canvas = tkinter.Canvas(root,background="white", width=400, height=400)
+    canvas.pack()
+    canvas.bind("<Motion>",paint)
+    line = canvas.create_line(100, 175, 100, 50, fill='lightblue',width=1)
+
+init_main_window()
 root.mainloop()
 print("Тут и сказочке конец")
