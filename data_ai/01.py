fc56d0f15a58d9c3393119118eecd08043ff2463
SergeyLyakhovetsky <lotar37@gmail.com>
2022-03-22 15:44:19 +0300

    data ai

---
+++ data_ai/01.py
@@ -0,0 +1,14 @@
+result  = []
+files = ["+.txt"]#,"+.txt","-.txt","=.txt"]
+x_arr = []
+y_arr = []
+for k in range(len(files)):
+    print(files[k])
+    f = open(files[k])
+    lines = f.readlines()
+    for line in lines:
+        # if line == "\n":
+        #     continue
+        x_arr.append([int(i) for i in line[1:-2].split(",")])
+        y_arr.append(10+k)
+print(len(x_arr))
